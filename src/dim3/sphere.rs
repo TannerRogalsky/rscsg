1a15dde43eea8143a56eb9e667f2aa248a6ed400
Tanner Rogalsky <tanner@tannerrogalsky.com>
2021-04-15 10:58:32 -0400

    fix sphere generation

--- src/dim3/sphere.rs
+++ src/dim3/sphere.rs
@@ -3,89 +3,37 @@ use dim3::{Polygon, Vector, Vertex};
 use {Unit, UNIT_PI};
 
 impl Csg {
-    pub fn sphere(radius: Unit, slices: usize, stacks: usize) -> Csg {
-        fn make_vertex(radius: Unit, theta: Unit, phi: Unit) -> Vertex {
-            let phi_sin = phi.sin();
-            let d = Vector(theta.cos() * phi_sin, theta.sin() * phi_sin, phi.cos());
+    pub fn sphere(center: Vector, radius: Unit, slices: usize, stacks: usize) -> Csg {
 
-            Vertex::new(d * radius, d)
-        }
+        let mut vertex = |theta: Unit, phi: Unit| -> Vertex {
+            let theta = theta * UNIT_PI * 2.;
+            let phi = phi * UNIT_PI;
+            let dir = Vector(
+                theta.cos() * phi.sin(),
+                phi.cos(),
+                theta.sin() * phi.sin()
+            );
+            Vertex::new(center + dir * radius, dir)
+        };
 
-        let mut polys: Vec<Polygon> = Vec::new();
-        let delta_theta = UNIT_PI * 2.0 / (slices as Unit);
-        let delta_phi = UNIT_PI / (stacks as Unit);
+        let mut polys = vec![];
+        for slice in 0..slices {
+            for stack in 0..stacks {
+                let i = slice as Unit;
+                let j = stack as Unit;
+                let fslices = slices as Unit;
+                let fstacks = stacks as Unit;
 
-        // Bottom and top
-        for i in 0..slices {
-            let i0 = i as Unit;
-            let i1 = i0 + 1.0;
-
-            // Bottom
-            //  +--+
-            //  | /
-            //  |/
-            //  +
-            polys.push(Polygon::new(vec![
-                make_vertex(radius, i0 * delta_theta, 0.0),
-                make_vertex(radius, i0 * delta_theta, delta_phi),
-                make_vertex(radius, i1 * delta_theta, delta_phi),
-            ]));
-
-            // Top
-            //  +
-            //  |\
-            //  | \
-            //  +--+
-            let j0 = (stacks as Unit) - 1.0;
-            let j1 = j0 + 1.0;
-
-            polys.push(Polygon::new(vec![
-                make_vertex(radius, i0 * delta_theta, j0 * delta_phi),
-                make_vertex(radius, i1 * delta_theta, j0 * delta_phi),
-                make_vertex(radius, i0 * delta_theta, j1 * delta_phi),
-            ]));
-        }
-
-        for j in 1..stacks - 1 {
-            let j0 = j as Unit;
-            let j1 = j0 + 0.5;
-            let j2 = j0 + 1.0;
-
-            // Between top and bottom
-            //  +---+
-            //  |\ /|
-            //  | x |
-            //  |/ \|
-            //  +---+
-
-            for i in 0..slices {
-                let i0 = i as Unit;
-                let i1 = i0 + 0.5;
-                let i2 = i0 + 1.0;
-
-                polys.push(Polygon::new(vec![
-                    make_vertex(radius, i1 * delta_theta, j1 * delta_phi),
-                    make_vertex(radius, i2 * delta_theta, j2 * delta_phi),
-                    make_vertex(radius, i0 * delta_theta, j2 * delta_phi),
-                ]));
-
-                polys.push(Polygon::new(vec![
-                    make_vertex(radius, i1 * delta_theta, j1 * delta_phi),
-                    make_vertex(radius, i0 * delta_theta, j0 * delta_phi),
-                    make_vertex(radius, i2 * delta_theta, j0 * delta_phi),
-                ]));
-
-                polys.push(Polygon::new(vec![
-                    make_vertex(radius, i1 * delta_theta, j1 * delta_phi),
-                    make_vertex(radius, i0 * delta_theta, j2 * delta_phi),
-                    make_vertex(radius, i0 * delta_theta, j0 * delta_phi),
-                ]));
-
-                polys.push(Polygon::new(vec![
-                    make_vertex(radius, i1 * delta_theta, j1 * delta_phi),
-                    make_vertex(radius, i2 * delta_theta, j0 * delta_phi),
-                    make_vertex(radius, i2 * delta_theta, j2 * delta_phi),
-                ]));
+                let mut vertices = vec![];
+                vertices.push(vertex(i / fslices, j / fstacks));
+                if stack > 0 {
+                    vertices.push(vertex((i + 1.) / fslices, j / fstacks));
+                }
+                if stack < stacks - 1 {
+                    vertices.push(vertex((i + 1.) / fslices, (j + 1.) / fstacks))
+                }
+                vertices.push(vertex(i / fslices, (j + 1.) / fstacks));
+                polys.push(Polygon::new(vertices));
             }
         }
 
